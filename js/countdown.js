b93cafc840ff577c6852dad421bc3cbb65b0adc0
Marina Chun <marinachun@gmail.com>
2021-06-02 10:47:12 +0900

    added christmas

--- js/countdown.js
+++ js/countdown.js
@@ -3,18 +3,20 @@ let hoursEl = document.getElementById("hours");
 let minutesEl = document.getElementById("minutes");
 let secondsEl = document.getElementById("seconds");
 
-let dateCountdown = "01-01-2022";
+let headingEl = document.getElementById("heading");
+let heading = "Countdown to New Years:";
+
+let countdownDate = "01-01-2022";
 
 let background = 'background/new-years.jpeg';
 
 function setBackground() {
     document.body.style.backgroundImage = "url(" + background + ")";
-
 }
 
 function countdown() {
     let currentDate = new Date();
-    let dateTo = new Date(dateCountdown);
+    let dateTo = new Date(countdownDate);
 
     const secondsTo = Math.floor((dateTo - currentDate)/1000);
 
@@ -32,5 +34,27 @@ function countdown() {
 setBackground();
 setInterval(countdown,1000);
 
+document.getElementById("countdown-type").onchange = updateCountdown;
 
+function updateCountdown() {
+    var selected = document.getElementById("countdown-type").value;
+    console.log(selected);
+    if (selected === "christmas") {
+        background = 'background/christmas.jpeg'
+        countdownDate = '12-25-2021';
+        title = "Countdown to Christmas:"
+    }
 
+    if (selected === "new-years") {
+        background = 'background/new-years.jpeg'
+        countdownDate = '01-01-2022';
+        title = "Countdown to New Years"
+    }
+    if (selected === "birthday") {
+        background = 'background/birthday.jpeg'
+        title = "Countdown to Birthday:"
+    }
+
+    headingEl.innerHTML = title;
+    setBackground();
+}
